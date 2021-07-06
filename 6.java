527ddd3ac58114ad9becde036cf5b9a42ce576a3
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2021-07-06 21:17:00 +0530

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+       int target = arr.length/2; 
+       int[] sizes = new int[100001];
+       int max_size = 0; 
+        
+       for(int i = 0;i<arr.length;i++){
+          sizes[arr[i]]+=1; 
+          if(sizes[i] >= target) return 1; 
+          if(max_size < arr[i]) max_size = arr[i]; 
+
+       } 
+        
+       int[] counts = new int[50001];
+       int max_count = 0;       
+       for(int i = 1; i <=max_size  ; i++){
+          counts[sizes[i]]+=1; 
+          if(max_count < sizes[i]) max_count = sizes[i]; 
+       } 
+        
+       
+       int currCutted = 0;
+       int currCuttedUniq = 0;
+        
+       for(int i = max_count; i > 0 ; i--){
+          if(target < currCutted + counts[i] * i){
+              return currCuttedUniq  + (target - currCutted+i-1)/i;
+          } 
+          currCutted += counts[i] * i;
+          currCuttedUniq += counts[i];
+              
+       }
+        
+       return 0; 
+    }
+}
